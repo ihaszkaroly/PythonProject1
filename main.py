da8e87b45d1b4a3b5bbae05db0018a38d20f55c3
ihaszkaroly <kroly99@gmail.com>
2025-09-04 09:47:45 +0200

    commit

--- main.py
+++ main.py
@@ -6,11 +6,11 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+    print(f'Szia, {name}')  # Press F9 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Szkript óra.')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
